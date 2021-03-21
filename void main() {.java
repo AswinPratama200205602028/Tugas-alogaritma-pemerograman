c5dac7f41ace272ef9fe5ee10daa4b2e85b7b7e2
AswinPratama200205602028 <aswinpratama1741@gmail.com>
2021-03-21 14:49:02 +0800

    menambahkan file

---
+++ void main() {.java
@@ -0,0 +1,19 @@
+void main() {
+  
+  //string = tipe data untuk kalimat
+  String nama = "Aswin Pratama";
+print ("nama : $nama ");
+  
+//int=tipe data untuk angka
+int  umur = 20;
+print ("umur : $umur");
+  
+//bool=tipe data untuk true dan false
+bool mahasiswa = true ;
+print ("mahasiswa : $mahasiswa");
+  
+//double=tipe data untuk bilangan berkoma
+double ipk = 3.9;
+print ("ipk : $ipk");
+
+  }
